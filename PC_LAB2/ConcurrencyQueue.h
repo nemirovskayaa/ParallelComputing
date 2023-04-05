79b8b70cfe62220e74647343fad33e1fee905b8c
nemirovskayaa <nemivika@gmail.com>
2023-04-05 15:07:01 +0300

    setting max size added;

--- PC_LAB2/ConcurrencyQueue.h
+++ PC_LAB2/ConcurrencyQueue.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <shared_mutex>
+#include <climits>
 
 using namespace std;
 
@@ -23,6 +24,7 @@ private:
 	};
 
 	unsigned int m_size;
+	unsigned int m_max_size;
 	Node* m_head;
 	Node* m_tail;
 	shared_mutex m_rw_mutex;
@@ -30,11 +32,12 @@ private:
 public:
 	ConcurrencyQueue();
 	~ConcurrencyQueue();
-	void push(T data);
+	bool push(T data);
 	T pop();
 	bool empty();
 	void clear();
 	unsigned int get_size();
+	void set_max_size(unsigned int);
 };
 
 template <typename T>
@@ -43,6 +46,7 @@ ConcurrencyQueue<T>::ConcurrencyQueue()
 	this->m_head = nullptr;
 	this->m_tail = nullptr;
 	this->m_size = 0;
+	this->m_max_size = UINT_MAX;
 }
 
 template <typename T>
@@ -52,8 +56,14 @@ ConcurrencyQueue<T>::~ConcurrencyQueue()
 }
 
 template <typename T>
-void ConcurrencyQueue<T>::push(T data)
+bool ConcurrencyQueue<T>::push(T data)
 {
+	{
+		shared_lock<shared_mutex> s_lock(m_rw_mutex);
+		if (m_size == m_max_size)
+			return false;
+	}
+
 	unique_lock<shared_mutex> u_lock(m_rw_mutex);
 	Node* new_node = new Node(nullptr, m_tail, data);
 	if (m_tail == nullptr)
@@ -67,6 +77,7 @@ void ConcurrencyQueue<T>::push(T data)
 		m_tail = new_node;
 	}
 	m_size++;
+	return true;
 }
 
 template <typename T>
@@ -122,4 +133,11 @@ unsigned int ConcurrencyQueue<T>::get_size()
 {
 	shared_lock<shared_mutex> s_lock(m_rw_mutex);
 	return m_size;
+}
+
+template <typename T>
+void ConcurrencyQueue<T>::set_max_size(unsigned int max_size)
+{
+	unique_lock<shared_mutex> u_lock(m_rw_mutex);
+	m_max_size = max_size;
 }
